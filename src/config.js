c550550ff839df0dc16799ff242ded9297c4c2dc
wanderingcrypto <91278375+wanderingcrypto@users.noreply.github.com>
2021-10-03 15:12:53 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,76 +3,16 @@
 const path = require("path");
 const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
-const { MODE } = require(path.join(basePath, "src/blendMode.js"));
-const description =
-  "Crypto Karens is a collection of 100 outraged, randomly generated NFTs that exist on the blockchain. Holders of Crypto Karens take part in the multi-meme-verse and future community drops and events. Be on the look-out as some are unique and more rare than others.";
-const baseUri = "ipfs://IPFS_URI_HERE";
-const metadataName = "Crypto Karens";
-
-const layerConfigurations = [
-  {
-    growEditionSizeTo: 100,
-    layersOrder: [
-      { name: "Background" },
-      { name: "Base" },
-      { name: "Eyes" },
-      { name: "Hair" },
-      { 
-        name: "Misc",
-        // todo: implement rules for misc ie; have a max number of traits
-        rules: {
-          min: 1,
-          max: 6
-        }
-      },
-      { name: "Mouth" },
-      { name: "Nose" },
-    ],
-    rarityPercent: 5
-  },
-];
-
-const uniques = [
-  {
-    name: "Rainbow Karen",
-    description: "\"Together we can make a difference.\" This Karen is experiencing an awakening...",
-    folder: "rares/RainbowKaren/"
-  },
-  {
-    name: "Zombie Karen",
-    description: "Brains.. brains.. can I speak to your overlord...",
-    folder: "rares/ZombieKaren/"
-  }
-];
-
-const shuffleLayerConfigurations = false;
-
-const debugLogs = false;
-
-const format = {
-  width: 512,
-  height: 512,
-};
-
-const background = {
-  generate: true,
-  brightness: "80%",
-};
-
-const extraMetadata = {};
-
-const rarityDelimiter = "#";
-
-const uniqueDnaTorrance = 10000;
-
-const preview = {
-  thumbPerRow: 5,
-  thumbWidth: 50,
-  imageRatio: format.width / format.height,
-  imageName: "preview.png",
-};
-
-module.exports = {
+const fs = require("fs");
+const sha1 = require(path.join(basePath, "/node_modules/sha1"));
+const { createCanvas, loadImage } = require(path.join(
+  basePath,
+  "/node_modules/canvas"
+));
+const buildDir = path.join(basePath, "/build");
+const layersDir = path.join(basePath, "/layers");
+console.log(path.join(basePath, "/src/config.js"));
+const {
   format,
   baseUri,
   description,
@@ -80,10 +20,328 @@ module.exports = {
   uniqueDnaTorrance,
   layerConfigurations,
   rarityDelimiter,
-  preview,
   shuffleLayerConfigurations,
   debugLogs,
   extraMetadata,
   uniques,
   metadataName
+} = require(path.join(basePath, "/src/config.js"));
+const canvas = createCanvas(format.width, format.height);
+const ctx = canvas.getContext("2d");
+var metadataList = [];
+var attributesList = [];
+var dnaList = [];
+var uniqueList = {};
+var uniquesFound = 0;
+require("crypto-js");
+
+const buildSetup = () => {
+  if (fs.existsSync(buildDir)) {
+    fs.rmdirSync(buildDir, { recursive: true });
+  }
+  fs.mkdirSync(buildDir);
+  fs.mkdirSync(path.join(buildDir, "/json"));
+  fs.mkdirSync(path.join(buildDir, "/images"));
 };
+
+const getRarityWeight = (_str) => {
+  let nameWithoutExtension = _str.slice(0, -4);
+  var nameWithoutWeight = Number(
+    nameWithoutExtension.split(rarityDelimiter).pop()
+  );
+  if (isNaN(nameWithoutWeight)) {
+    nameWithoutWeight = 0;
+  }
+  return nameWithoutWeight;
+};
+
+const cleanDna = (_str) => {
+  var dna = Number(_str.split(":").shift());
+  return dna;
+};
+
+const cleanName = (_str) => {
+  let nameWithoutExtension = _str.slice(0, -4);
+  var nameWithoutWeight = nameWithoutExtension.split(rarityDelimiter).shift();
+  return nameWithoutWeight;
+};
+
+const getElements = (path) => {
+  return fs
+    .readdirSync(path)
+    .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+    .map((i, index) => {
+      return {
+        id: index,
+        name: cleanName(i),
+        filename: i,
+        path: `${path}${i}`,
+        weight: getRarityWeight(i),
+      };
+    });
+};
+
+const layersSetup = (layersOrder, customLayersDir) => {
+  const layers = layersOrder.map((layerObj, index) => ({
+    id: index,
+    name: layerObj.name,
+    elements: getElements(`${layersDir}/${customLayersDir || ""}${layerObj.name}/`),
+    blendMode:
+      layerObj["blend"] != undefined ? layerObj["blend"] : "source-over",
+    opacity: layerObj["opacity"] != undefined ? layerObj["opacity"] : 1,
+  }));
+  return layers;
+};
+
+const saveImage = (_editionCount) => {
+  fs.writeFileSync(
+    `${buildDir}/images/${_editionCount}.png`,
+    canvas.toBuffer("image/png")
+  );
+};
+
+const genColor = () => {
+  let hue = Math.floor(Math.random() * 360);
+  let pastel = `hsl(${hue}, 100%, ${background.brightness})`;
+  return pastel;
+};
+
+const drawBackground = () => {
+  ctx.fillStyle = genColor();
+  ctx.fillRect(0, 0, format.width, format.height);
+};
+
+const addMetadata = (_dna, _edition, _custom) => {
+
+  if(_custom) {
+    attributesList = [
+      {
+        trait_type: 'custom',
+        value: 'Legendary'
+      },
+      {
+        trait_type: 'name',
+        value: _custom.name
+      },
+    ];
+  }
+
+  //let dateTime = Date.now();
+  let tempMetadata = {
+    //dna: sha1(_dna.join("")),
+    name: metadataName + ` #${_edition}`,
+    description: description,
+    image: `${baseUri}/${_edition}.png`,
+    // edition: _edition,
+    //date: dateTime,
+    //...extraMetadata,
+    attributes: attributesList,
+    //compiler: "HashLips Art Engine",
+  };
+  metadataList.push(tempMetadata);
+  attributesList = [];
+};
+
+const addAttributes = (_element) => {
+  let selectedElement = _element.layer.selectedElement;
+  attributesList.push({
+    trait_type: _element.layer.name,
+    value: selectedElement.name,
+  });
+};
+
+const loadLayerImg = async (_layer) => {
+  return new Promise(async (resolve) => {
+    const image = await loadImage(`${_layer.selectedElement.path}`);
+    resolve({ layer: _layer, loadedImage: image });
+  });
+};
+
+const drawElement = (_renderObject) => {
+  ctx.globalAlpha = _renderObject.layer.opacity;
+  ctx.globalCompositeOperation = _renderObject.layer.blendMode;
+  ctx.drawImage(_renderObject.loadedImage, 0, 0, format.width, format.height);
+  addAttributes(_renderObject);
+};
+
+const constructLayerToDna = (_dna = [], _layers = []) => {
+  let mappedDnaToLayers = _layers.map((layer, index) => {
+    let selectedElement = layer.elements.find(
+      (e) => e.id == cleanDna(_dna[index])
+    );
+    return {
+      name: layer.name,
+      blendMode: layer.blendMode,
+      opacity: layer.opacity,
+      selectedElement: selectedElement,
+    };
+  });
+  return mappedDnaToLayers;
+};
+
+const isDnaUnique = (_DnaList = [], _dna = []) => {
+  let foundDna = _DnaList.find((i) => i.join("") === _dna.join(""));
+  return foundDna == undefined ? true : false;
+};
+
+const createDna = (_layers) => {
+  let randNum = [];
+  _layers.forEach((layer) => {
+    var totalWeight = 0;
+    layer.elements.forEach((element) => {
+      totalWeight += element.weight;
+    });
+    // number between 0 - totalWeight
+    let random = Math.floor(Math.random() * totalWeight);
+    for (var i = 0; i < layer.elements.length; i++) {
+      // subtract the current weight from the random weight until we reach a sub zero value.
+      random -= layer.elements[i].weight;
+      if (random < 0) {
+        return randNum.push(
+          `${layer.elements[i].id}:${layer.elements[i].filename}`
+        );
+      }
+    }
+  });
+  return randNum;
+};
+
+const writeMetaData = (_data) => {
+  fs.writeFileSync(`${buildDir}/json/_metadata.json`, _data);
+};
+
+const saveMetaDataSingleFile = (_editionCount) => {
+  //let metadata = metadataList.find((meta) => meta.edition == _editionCount);
+  let metadata = metadataList[_editionCount - 1];
+  debugLogs
+    ? console.log(
+      `Writing metadata for ${_editionCount}: ${JSON.stringify(metadata)}`
+    )
+    : null;
+  fs.writeFileSync(
+    `${buildDir}/json/${_editionCount}.json`,
+    JSON.stringify(metadata, null, 2)
+  );
+};
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+function getAvailableUnique() {
+  let random = Math.floor(Math.random() * 100) % uniques.length;
+  if(uniqueList[random]) {
+    while(uniqueList[random]) {
+      random = Math.floor(Math.random() * 100) % uniques.length;
+    }
+  }
+  uniqueList[random] = true;
+  return uniques[random];
+}
+
+const startCreating = async () => {
+  let layerConfigIndex = 0;
+  let editionCount = 1;
+  let failedCount = 0;
+  let abstractedIndexes = [];
+  for (
+    let i = 1;
+    i <= layerConfigurations[layerConfigurations.length - 1].growEditionSizeTo;
+    i++
+  ) {
+    abstractedIndexes.push(i);
+  }
+  if (shuffleLayerConfigurations) {
+    abstractedIndexes = shuffle(abstractedIndexes);
+  }
+  debugLogs
+    ? console.log("Editions left to create: ", abstractedIndexes)
+    : null;
+  while (layerConfigIndex < layerConfigurations.length) {
+    const layers = layersSetup(
+      layerConfigurations[layerConfigIndex].layersOrder
+    );
+    while (
+      editionCount <= layerConfigurations[layerConfigIndex].growEditionSizeTo
+    ) {
+
+      // check for unique
+      let random = Math.floor(Math.random() * 100);
+      let rare = null;
+      let isRare = false;
+      let rareLayers = {};
+
+      if (uniquesFound < uniques.length && random <= layerConfigurations[layerConfigIndex].rarityPercent) {
+        isRare = true;
+        rare = getAvailableUnique();
+        console.log(`Rare found, ${rare.name}!`)
+        rareLayers = layersSetup(
+          layerConfigurations[layerConfigIndex].layersOrder,
+          rare.folder
+        );
+      }
+
+      let activeLayer = !isRare ? layers : rareLayers;
+
+      let newDna = createDna(activeLayer);
+      if (isDnaUnique(dnaList, newDna)) {
+        let results = constructLayerToDna(newDna, activeLayer);
+        let loadedElements = [];
+
+        results.forEach((layer) => {
+          loadedElements.push(loadLayerImg(layer));
+        });
+
+        await Promise.all(loadedElements).then((renderObjectArray) => {
+          debugLogs ? console.log("Clearing canvas") : null;
+          ctx.clearRect(0, 0, format.width, format.height);
+          if (background.generate) {
+            drawBackground();
+          }
+          renderObjectArray.forEach((renderObject) => {
+            drawElement(renderObject);
+          });
+          debugLogs
+            ? console.log("Editions left to create: ", abstractedIndexes)
+            : null;
+          const index = editionCount; // abstractedIndex[0];
+          saveImage(index);
+          addMetadata(newDna, index, rare);
+          saveMetaDataSingleFile(index);
+          console.log(
+            `Created edition: ${index}, with DNA: ${sha1(
+              newDna.join("")
+            )}`
+          );
+        });
+        dnaList.push(newDna);
+        editionCount++;
+        abstractedIndexes.shift();
+        if(isRare) uniquesFound++;
+      } else {
+        console.log("DNA exists!");
+        failedCount++;
+        if (failedCount >= uniqueDnaTorrance) {
+          console.log(
+            `You need more layers or elements to grow your edition to ${layerConfigurations[layerConfigIndex].growEditionSizeTo} artworks!`
+          );
+          process.exit();
+        }
+      }
+    }
+    layerConfigIndex++;
+  }
+  writeMetaData(JSON.stringify(metadataList, null, 2));
+};
+
+module.exports = { startCreating, buildSetup, getElements };
